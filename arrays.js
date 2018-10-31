95c254e5f067385fdaab665804eb965ee740edbe
akedalat <akedalat@gmail.com>
2018-10-31 22:45:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -23,5 +23,8 @@ function destructivelyRemoveElementFromBeginningOfArray (array){
 function removeElementFromBeginningOfArray (array){
   array.slice(1)
   return array
-
+}
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
 }
